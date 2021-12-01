d75c274b342e0d4f2491d11d3614dfe49a0ca87f
Giovanni Gasparini <giovannigasparini@ucsb.edu>
2021-11-30 18:15:24 -0800

    new files

--- moreArrayFuncs.cpp
+++ moreArrayFuncs.cpp
@@ -1,6 +1,6 @@
 #include <cassert>
 #include "moreArrayFuncs.h"
-
+#include <cstdlib>
 // a: an array of ints.  size is how many ints in array
 // Return the index of largest value.  
 // If more than one element has largest value, 
@@ -10,7 +10,19 @@
 
 int indexOfMax(int *a, int size) {
   assert(size >= 1);
-  return -42; // STUB !!! Remove and replace with correct code
+  int max = -100000;
+  for (int i =0; i< size; i++){
+	  if (a[i]>max){
+		  max = a[i];
+	  }
+  }
+  for (int i = 0;i<size; i++){
+	  if (a[i] ==max){
+		  return i;
+	  }
+  }
+  return -1;
+  // STUB !!! Remove and replace with correct code
 }
 
 // a: an array of ints.  size is how many ints in array
@@ -21,7 +33,18 @@ int indexOfMax(int *a, int size) {
 // You may assume size >= 1
 int indexOfMin(int *a, int size) {
   assert(size >= 1);
-  return -42; // STUB !!! Remove and replace with correct code
+  int min = 100000;
+  for (int i =0; i< size; i++){
+          if (a[i]<min){
+                  min = a[i];
+          }
+  }
+  for (int i = 0;i<size; i++){
+          if (a[i] ==min){
+                  return i;
+          }
+  }
+  return -1; // STUB !!! Remove and replace with correct code
 }
 
 // a: an array of ints.  size is how many ints in array
@@ -30,7 +53,13 @@ int indexOfMin(int *a, int size) {
 // You may assume size >= 1
 int largestValue(int *a, int size) {
   assert(size >= 1);
-  return -42; // STUB !!! Remove and replace with correct code
+  int max = -100000;
+  for (int i =0; i< size; i++){
+          if (a[i]>max){
+                  max = a[i];
+          }
+  }
+  return max; // STUB !!! Remove and replace with correct code
 }
 
 // a: an array of ints.  size is how many ints in array
@@ -40,7 +69,13 @@ int largestValue(int *a, int size) {
 
 int smallestValue(int *a, int size) {
   assert(size >= 1);
-  return -42; // STUB !!! Remove and replace with correct code
+  int min = 100000;
+  for (int i =0; i< size; i++){
+          if (a[i]<min){
+                  min = a[i];
+          }
+  }
+  return min; // STUB !!! Remove and replace with correct code
 }
 
 
@@ -49,7 +84,12 @@ int smallestValue(int *a, int size) {
 // size may be 0 in this case, or non-zero.
 
 int sum(int *a, int size) {
-  return -42; // STUB
+  int sum = 0;
+  for (int i =0; i< size; i++){
+    sum += a[i];
+  }
+  return sum;
+ // STUB
 }
 
 
@@ -60,7 +100,9 @@ int sum(int *a, int size) {
 // copy n elements from src to dest.
 
 void copyElements(int *dest, int *src, int n) {
-  // STUB: Fix this
+  for (int i =0; i<n;i++){
+    dest[i] = src[i];
+  }
 }
 
 // dest is an array of int that has capacity at LEAST of size n
@@ -71,10 +113,17 @@ void copyElements(int *dest, int *src, int n) {
 // return the number of elements that were copied
 
 int copyOddOnly(int *dest, int *src, int n) {
-  return -42; // stub @@@ FIX THIS 
+  int numOdds=0;
+  for (int i =0; i<n;i++){
+    if (((abs(src[i])) %2)==1){
+      dest[numOdds] = src[i];
+      numOdds++;
+  } // stub @@@ FIX THIS 
+  }
+  return numOdds;
 }
 
-// a, b and product are all arrays of size n (or greater)
+// a, b and puct are all arrays of size n (or greater)
 // in each case, only the first n elements of a, b and product are used
 // a and b have their values read, and are unchanged
 // the old values in product are ignored, and overwritten.
@@ -86,13 +135,16 @@ int copyOddOnly(int *dest, int *src, int n) {
 // product will be {3,10,21,36}.
 
 void multiplyPairwise(int *a, int *b, int *product, int n) {
-  // STUB; fix this
+  for (int i =0; i<n;i++){
+    product[i] = a[i] * b[i];
+  }
 }
 
 // len is the number of elements the array you create should contain
 // you should return a pointer to the new integer array of size len
 // with all of the elements in it set to 0
 int* createArray(int len) {
-  //STUB; fix this
-  return 0;
+  int newArray [len] = { };
+  int (*returnArray)[len] = &newArray;
+  return returnArray;
 }
